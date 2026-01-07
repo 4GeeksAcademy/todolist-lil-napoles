d0607f3a9ca2a774b201d3f1069ce25b879142e9
lilnapolesbase9-maker <lilnapolesbase9@gmail.com>
2026-01-07 18:56:51 +0000

    mentoria

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [toDoList, setToDoList] = useState([])
+	const [input, setInput] = useState("hola")
+	function handleSubmit(e) {
+		e.preventDefault()
+
+
+		const newTask = {
+
+			label: input,
+			is_done: false
+		}
+		setToDoList([...toDoList, newTask]);
+		setInputValue(""); //
+	}
+
 	return (
 		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="titulo">to do list</h1>
+			<form className="form" onSubmit={handleSubmit} >
+				<input type="text" value={input} onChange={(e) => { setInput(e.target.value) }
+				} />
+			</form>
+			<div className="renderizado">
+
+
+			</div>
 		</div>
 	);
 };
